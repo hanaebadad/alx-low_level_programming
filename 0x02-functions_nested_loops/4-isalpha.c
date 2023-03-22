13956c0bec6ead922da2ffeafd518efbc64d88c2
hanaebadad <badad.hanae@gmail.com>
2023-03-21 18:47:01 -0700

    dh

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isalpha - check if a character is an alphabet character
- *
- * @c: characcter to check
- *
- * Return: 0 or 1
- *
+ * _isalpha - checks for alphabetic character
+ * @c: The character to be checked
+ * Return: 1 for alphabetic character or 0 for anything else
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-		return (1);
-	else
-		return (0);
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+return (0);
 }
