46411cfb9c87b345fe02a0cec09da10d18b0e05a
hanaebadad <badad.hanae@gmail.com>
2023-03-27 14:39:01 -0700

    jsfdb

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - a func that takes a pointer to an
+ * int & updates its value to 98
+ *
+ * @n: integer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
